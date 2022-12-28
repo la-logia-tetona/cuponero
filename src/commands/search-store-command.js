eaf3a9878a248296750364e60f0a5ce9947dc626
cucchi10 <cucchi.sm@hotmail.com>
2022-12-28 18:17:44 -0300

    se hace una ternaria de mierda para buscar tiendas por ID y por nombre (pero funciona)

--- src/commands/search-store-command.js
+++ src/commands/search-store-command.js
@@ -1,5 +1,6 @@
 const { StoreDAO } = require('../db/dao/store-dao');
 const { Command } = require('./command');
+const { isNumber } = require('../utils/number');
 
 class SearchStoreCommand extends Command {
 	constructor(command) {
@@ -10,20 +11,30 @@ class SearchStoreCommand extends Command {
 	async handleInteraction(interaction) {
 		if (interaction.commandName === 'buscar') {
 			const storeName = interaction.options.getString('tienda');
-			const stores = await this.storeDAO.findStoreNameLike(storeName);
+			let stores = null;
+			if (isNumber(storeName)) {
+				stores = await this.storeDAO.getStoreById(storeName);
+			}
+			else {
+				stores = await this.storeDAO.findStoreNameLike(storeName);
+			}
 			if (!stores) {
 				Command.reply(interaction, 'Ocurrió un error al buscar las tiendas');
 			}
 			else if (stores.length === 0) {
-				const message = storeName ?
-					'No existen tiendas con nombre parecido a ' + storeName :
-					'No hay tiendas en el cuponero';
+				const message = isNumber(storeName) ?
+					'No existen tiendas con ID ' + storeName :
+					storeName ?
+						'No existen tiendas con nombre parecido a ' + storeName :
+						'No hay tiendas en el cuponero';
 				Command.reply(interaction, message);
 			}
 			else {
-				const message = storeName ?
-					'Tiendas con nombre parecido a ' + storeName :
-					'Listando todas las tiendas del cuponero';
+				const message = isNumber(storeName) ?
+					'Tienda con ID ' + storeName :
+					storeName ?
+						'Tiendas con nombre parecido a ' + storeName :
+						'Listando todas las tiendas del cuponero';
 				await Command.reply(interaction, message);
 				this.toReplyStrings(stores).forEach(replyString => interaction.channel.send(replyString));
 			}
